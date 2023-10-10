f5732fefe62600bdb7fbab717bed53743315bd6b
BuildTools <mariothrowsfieball@gmail.com>
2023-10-10 17:55:18 +0200

    uploding to github

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
 
 app.use(express.static("static"));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
    console.log("server started on port 3000");
 });
