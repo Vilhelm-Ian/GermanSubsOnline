5d0d3959db1c00ce98d19215193ddc1fa80c0428
BuildTools <mariothrowsfieball@gmail.com>
2023-10-10 18:10:46 +0200

    renamed static folder

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.use(express.static("static"));
+app.use(express.static("public"));
 
 app.listen(process.env.PORT || 3000, () => {
    console.log("server started on port 3000");
