646077fbfd388d341724c5d310d26b3bd34eafec
BuildTools <mariothrowsfieball@gmail.com>
2023-10-10 18:20:03 +0200

    pushing just to redeploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
