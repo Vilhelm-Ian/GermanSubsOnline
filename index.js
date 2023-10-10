aa4e6d21b293ec6c567c5fb7b1cd4641b1baa7ec
BuildTools <mariothrowsfieball@gmail.com>
2023-10-10 18:59:00 +0200

    .

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-//app.get("/", (_req, res) => {
-//   res.sendFile(path.join(__dirname, "/index.html"));
-//});
+app.get("/", (_req, res) => {
+   res.sendFile(path.join(__dirname, "/index.html"));
+});
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 app.listen(process.env.PORT || 3000, () => {
    console.log("server started on port 3000");
